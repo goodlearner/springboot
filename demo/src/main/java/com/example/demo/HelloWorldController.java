0d43f2ac945619eb49729833cb3238b6a450e816
Anyho <wuh@infoepoch.com>
2019-03-07 23:20:17 +0800

    add HelloWorldController and test functions

---
+++ demo/src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by anyho on 2019/3/7.
+ */
+@RestController
+public class HelloWorldController
+{
+    @RequestMapping("/hello")
+    public String index()
+    {
+        return "Hello SpringBoot!";
+    }
+}
